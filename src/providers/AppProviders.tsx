d134c2487099498d0ef2648f836d6d44c15732da
nebular-lab <hiranodaisuke0721@gmail.com>
2023-09-10 08:30:17 +0900

    tanstack queryのprovider

---
+++ src/providers/AppProviders.tsx
@@ -0,0 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const AppProviders = ({ children }: Props) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        suspense: true,
+      },
+    },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
